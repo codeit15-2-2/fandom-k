4ece7fb01be8d26676a208d82e90798e32fccda7
MyungJiwoo <1206jiwoo@gmail.com>
2025-04-18 00:22:23 +0900

    #8 refactor(IdolList): 프로필 이미지 공통 컴포넌트 적용 및 IdolList 컴포넌트 사용법 추가

--- src/components/vote/IdolList.jsx
+++ src/components/vote/IdolList.jsx
@@ -1,20 +1,10 @@
 import { useContext, createContext } from 'react';
 import Selected from './Selected';
-
-/** 아이돌 리스트 컴포넌트 props (기본형과 투표형을 함께 사용하므로 자세한 사용법은 파일 마지막을 참고해주세요.)
- * @param {number} id - [기본형 | 투표형] 아이돌 id
- * @param {string} imgUrl - [기본형 | 투표형] 이미지
- * @param {string} rank - [기본형 | 투표형] 순위
- * @param {string} group - [기본형 | 투표형] 아이돌 그룹
- * @param {string} name - [기본형 | 투표형] 아이돌 이름
- * @param {number} votes - [기본형 | 투표형] 투표 수 (string으로 사용 시, 포맷 설정 함수에서 오류가 발생합니다.)
- * @param {number} selectedId - [투표형] 현재 선택된 아이돌 id
- * @param {function} onChange - [투표형] selectedId 변경시 값 저장 이벤트 핸들러
- */
+import ProfileImg from '@components/common/ProfileImg';
 
 const IdolContext = createContext({
   id: null,
-  imgUrl: '',
+  src: '',
   rank: '',
   group: '',
   name: '',
@@ -23,10 +13,72 @@ const IdolContext = createContext({
   onChange: () => {},
 });
 
-// 아이돌 리스트 컴포넌트 (외부에서 import하여 사용)
+/* 아이돌 리스트 컴포넌트 사용법
+- 소개
+아이돌 리스트 컴포넌트는 합성 컴포넌트로, 원하는 컴포넌트를 외부에서 합성해서 사용할 수 있습니다.
+합성 대상은 IdolNameWithGroup와 IdolVoteCount, IdolSelectRadio입니다.
+
+
+- 반응형 사용법
+기본 width가 100%이고, margin은 0으로 설정되어 있습니다. 외부에서 레이아웃과 컴포넌트 간 여백을 설정하고 사용해주세요.
+*/
+
+/**
+ * 아이돌 리스트 컴포넌트 props (기본형과 투표형을 함께 사용하므로 아래의 자세한 사용법을 참고해주세요.)
+ * @param {Object} props
+ * @param {number} props.id - [기본형 | 투표형] 아이돌 id
+ * @param {string} props.src - [기본형 | 투표형] 이미지
+ * @param {number} props.rank - [기본형 | 투표형] 순위
+ * @param {string} props.group - [기본형 | 투표형] 아이돌 그룹
+ * @param {string} props.name - [기본형 | 투표형] 아이돌 이름
+ * @param {number} props.votes - [기본형 | 투표형] 투표 수 (string으로 사용 시, 포맷 설정 함수에서 오류가 발생합니다.)
+ * @param {number} [props.selectedId] - [투표형] 현재 선택된 아이돌 id (선택적)
+ * @param {function} [props.onChange] - [투표형] selectedId 변경시 값 저장 이벤트 핸들러 (선택적)
+ */
+/**
+ * @example
+ * 1. 기본형 사용법 예시
+ * selectedId와 onChange를 제외하고 props을 넘겨줍니다. (투표형 props이 없어도 오류나지 않습니다.)
+ * 내부에서 프로필 사진과 순위는 고정되어있습니다. 따라서 외부에서 IdolNameWithGroup와 IdolVoteCount만 합성합니다.
+ *
+ * <IdolList
+ *   id={1}
+ *   src={'~'}
+ *   rank={1}
+ *   group={'뉴진스'}
+ *   name={'민지'}
+ *   votes={240000}
+ * >
+ *   <IdolList.IdolNameWithGroup />
+ *   <IdolList.IdolVoteCount />
+ * </IdolList>
+ *
+ * @example
+ * 2. 투표형 사용법 예시
+ * selectedId와 onChange를 포함해 props을 넘겨줍니다. onChange는 selectedId를 변경하는 이벤트 핸들러입니다.
+ * 현재 투표형 디자인은 아이돌 이름과 투표 수가 세로 정렬이 되어야 하므로, <div className='flex flex-col'>으로 묶어줍니다.
+ * 외부에서 IdolNameWithGroup와 IdolVoteCount, IdolSelectRadio을 합성합니다.
+ *
+ * <IdolList
+ *   id={10}
+ *   src={'~'}
+ *   rank={1}
+ *   group={'뉴진스'}
+ *   name={'민지'}
+ *   votes={240003}
+ *   selectedId={selectedId}
+ *   onChange={onSelectIdol}
+ * >
+ *   <div className='flex flex-col'>
+ *     <IdolList.IdolNameWithGroup />
+ *     <IdolList.IdolVoteCount />
+ *   </div>
+ *   <IdolList.IdolSelectRadio />
+ * </IdolList>
+ */
 const IdolList = ({
   id,
-  imgUrl,
+  src,
   rank,
   group,
   name,
@@ -37,7 +89,7 @@ const IdolList = ({
 }) => {
   const contextValue = {
     id,
-    imgUrl,
+    src,
     rank,
     group,
     name,
@@ -59,15 +111,12 @@ const IdolList = ({
 
 // 아이돌 이미지 컴포넌트
 const IdolImg = ({ selected }) => {
-  const { imgUrl } = useContext(IdolContext);
+  const { src } = useContext(IdolContext);
 
   return (
-    <div className='border-brand-1 aspect-square w-20 rounded-full border-1 object-cover p-1'>
-      <div className='relative h-full w-full overflow-hidden rounded-full'>
-        <img className='block h-full w-full object-cover' src={imgUrl} />
-        {selected && <Selected />}
-      </div>
-    </div>
+    <ProfileImg src={src} size='s'>
+      {selected && <Selected />}
+    </ProfileImg>
   );
 };
 
@@ -121,53 +170,3 @@ IdolList.IdolVoteCount = IdolVoteCount;
 IdolList.IdolSelectRadio = IdolSelectRadio;
 
 export default IdolList;
-
-/* 아이돌 리스트 컴포넌트 사용법
-0. 소개
-아이돌 리스트 컴포넌트는 합성 컴포넌트로, 원하는 컴포넌트를 외부에서 합성해서 사용할 수 있습니다.
-합성 대상은 IdolNameWithGroup와 IdolVoteCount, IdolSelectRadio입니다.
-
-
-1. 반응형 사용법
-기본 width가 100%이고, margin은 0으로 설정되어 있습니다. 외부에서 레이아웃과 컴포넌트 간 여백을 설정하고 사용해주세요.
-
-
-2. 기본형 사용법 예시
-  ===> selectedId와 onChange를 제외하고 props을 넘겨줍니다. (투표형 props이 없어도 오류나지 않습니다.)
-  ===> 내부에서 프로필 사진과 순위는 고정되어있습니다. 따라서 외부에서 IdolNameWithGroup와 IdolVoteCount만 합성합니다.
-
-  <IdolList
-    id={1}
-    imgUrl={'~'}
-    rank={1}
-    group={'뉴진스'}
-    name={'민지'}
-    votes={240000}
-  >
-    <IdolList.IdolNameWithGroup />
-    <IdolList.IdolVoteCount />
-  </IdolList>
-
-
-3. 투표형 사용법 예시
-  ===> selectedId와 onChange를 포함해 props을 넘겨줍니다. onChange는 selectedId를 변경하는 이벤트 핸들러입니다.
-  ===> 현재 투표형 디자인은 아이돌 이름과 투표 수가 세로 정렬이 되어야 하므로, <div className='flex flex-col'>으로 묶어줍니다.
-  ===> 외부에서 IdolNameWithGroup와 IdolVoteCount, IdolSelectRadio을 합성합니다.
-
-  <IdolList
-    id={10}
-    imgUrl={'~'}
-    rank={1}
-    group={'뉴진스'}
-    name={'민지'}
-    votes={240003}
-    selectedId={selectedId}
-    onChange={onSelectIdol}
-  >
-    <div className='flex flex-col'>
-      <IdolList.IdolNameWithGroup />
-      <IdolList.IdolVoteCount />
-    </div>
-    <IdolList.IdolSelectRadio />
-  </IdolList>
-*/
