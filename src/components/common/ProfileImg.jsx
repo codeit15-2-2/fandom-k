67e75f7be9723200a1df08ccbdb8589e7702f098
MyungJiwoo <1206jiwoo@gmail.com>
2025-04-18 00:20:45 +0900

    #8 feat: 프로필 사진 공통 컴포넌트

---
+++ src/components/common/ProfileImg.jsx
@@ -0,0 +1,40 @@
+import { cn } from '@/utils/cn';
+
+const PROFILE_IMG_SIZE_STYLES = {
+  s: 'w-[7rem]',
+  m: 'w-[10rem]',
+  l: 'w-[12rem]',
+};
+
+/** 프로필 이미지 컴포넌트
+ * @param {Object} props
+ * @param {string} props.src - 이미지 URL
+ * @param {'s'|'m'|'l'} [props.size='s'] - 프로필 이미지 크기 (s=70px, m=100px, l=120px)
+ * @param {React.ReactNode} [props.children] - 이미지 위에 겹쳐질 추가 요소 (예: Selected 레이어 등)
+ *
+ * @example
+ * <ProfileImg src={'~'} size="m" />
+ * @example
+ * <ProfileImg src={imgUrl} size="s">{selected && <Selected />}</ProfileImg>
+ */
+const ProfileImg = ({ src, size = 's', children }) => {
+  const profileImgClassNames = cn(
+    'border-brand-1 aspect-square rounded-full border-1 p-1',
+    PROFILE_IMG_SIZE_STYLES[size],
+  );
+
+  return (
+    <div className={profileImgClassNames}>
+      <div className='relative h-full w-full overflow-hidden rounded-full'>
+        <img
+          className='block h-full w-full object-cover'
+          src={src}
+          alt='profile image'
+        />
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default ProfileImg;
