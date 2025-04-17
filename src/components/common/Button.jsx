05ed671b628e415c625a877ff08ef37d98465077
evaain706 <evaain706@gmail.com>
2025-04-17 18:04:59 +0900

    #9 refactor(button) 스타일관련 값 외부에서 상수로 관리/button type prop추가포함 리팩터"

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { cn } from '@/utils/cn';
+import {
+  SIZE_STYLES,
+  COLOR_STYLES,
+  DISABLED_STYLE,
+} from '@constants/buttonConstants';
 /**
  * Button 컴포넌트
  *
@@ -12,6 +17,7 @@ import { cn } from '@/utils/cn';
  * @param {boolean} [border=false] - 버튼에 border를 부여할지(border를 주고싶으면 props를 넘길때 border라고만 적기)
  * @param {boolean} [disabled=false] - 버튼 비활성화 여부(props를 줄때 disabled라고만 적기)
  * @param {boolean} [rounded=false] - 버튼이 둥근형태일지 여부(props를 줄때 rounded라고만 적기)
+ * @param {string} [btnType='button'] - 버튼의 type 속성 default는 'button' 
  *
  * @returns {JSX.Element} <Button/>컴포넌트
  * 
@@ -24,6 +30,7 @@ import { cn } from '@/utils/cn';
         size='xs'
         disabled
         rounded
+        btnType='submit'
         onClick={handleTest}
       />
 
@@ -38,48 +45,47 @@ import { cn } from '@/utils/cn';
  */
 const Button = ({
   btnText, //버튼 텍스트
+
   icon, //아이콘 유무
+
   size = 'xl', //버튼 사이즈
+
   color = 'pink', //색상
-  onClick, //onclick함수
+
+  onClick = () => {
+    console.log('TestClick');
+  }, //onclick함수
+
   className = '', //추가 className(스타일)
+
   border = false, //border여부
+
   disabled = false, //버튼 비활성 여부
+
   rounded = false, // 라운드 여부
+
+  btnType = 'button', //submit,button등 type여부
 }) => {
-  //사이즈
-  const sizeStyles = {
-    xl: 'h-[43px] w-[477px] sub-content-text text-bold py-[9px]',
-    l: 'h-[42px] w-[327px] sub-content-text text-bold py-[9px]',
-    m: 'h-[42px] w-[295px] sub-content-text text-bold py-[9px]',
-    s: 'h-[40px] w-[234px] sub-content-text text-bold py-[8px]',
-    xs: 'h-[31px] w-[142px] sub-content-text text-bold py-[6px]',
-  };
-
-  //컬러
-  const colorStyles = {
-    gray: 'bg-gray-200 text-white cursor-pointer opacity-80',
-    pink: 'bg-gradient-brand text-white cursor-pointer',
-  };
-
-  //disabled가 props로 넘어올시 적용할 스타일
-  const disabledStyles =
-    'bg-gray-300 text-white cursor-not-allowed pointer-events-none';
-
   //디폴트클래스네임 + size + disabled true시 스타일 + rounded true시 스타일 + border true시 스타일+ 추가 클래스네임 prop
-  const InitialClassName = cn(
+
+  const buttonClassNames = cn(
     'flex items-center justify-center transition-all font-bold', //디폴트 클래스네임
-    sizeStyles[size],
-    disabled ? disabledStyles : colorStyles[color],
+    SIZE_STYLES[size],
+    disabled ? DISABLED_STYLE : COLOR_STYLES[color],
     rounded ? 'rounded-full' : 'rounded-sm',
-    border && 'border-1 border-white',
+    border && 'border',
     className,
   );
 
   return (
-    <button onClick={onClick} className={InitialClassName} disabled={disabled}>
+    <button
+      type={btnType}
+      onClick={onClick}
+      className={buttonClassNames}
+      disabled={disabled}
+    >
       {icon && (
-        <span className='flex items-center justify-center object-contain'>
+        <span className='mr-2 flex items-center justify-center object-contain'>
           {icon}
         </span>
       )}
---
+++ src/constants/buttonConstants.js
@@ -0,0 +1,15 @@
+const SIZE_STYLES = {
+  xl: 'h-[43px] w-[477px] sub-content-text text-bold py-[9px]',
+  l: 'h-[42px] w-[327px] sub-content-text text-bold py-[9px]',
+  m: 'h-[42px] w-[295px] sub-content-text text-bold py-[9px]',
+  s: 'h-[40px] w-[234px] sub-content-text text-bold py-[8px]',
+  xs: 'h-[31px] w-[142px] sub-content-text text-bold py-[6px]',
+};
+const COLOR_STYLES = {
+  gray: 'bg-gray-200 text-white cursor-pointer opacity-80',
+  pink: 'bg-gradient-brand text-white cursor-pointer',
+};
+const DISABLED_STYLE =
+  'bg-gray-300 text-white cursor-not-allowed pointer-events-none';
+
+export { SIZE_STYLES, COLOR_STYLES, DISABLED_STYLE };
