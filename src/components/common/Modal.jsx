4d1aec707acef5a4ca4fa77022c99d1e3feb5dc1
Yongmin0423 <yym21345@gmail.com>
2025-04-17 15:05:08 +0900

    feat: 모달창 컴포넌트 스타일 변경

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,27 +1,35 @@
 import { createPortal } from 'react-dom';
 
-const Modal = ({
-  title = '타이틀',
-  button,
-  children,
-  extra,
-  isOpen,
-  onClose,
-}) => {
+/* 사용방법
+1. 모달을 사용하고 싶은 곳에 import한다.
+2. <Modal>콘텐츠 내용</Modal> 을 입력한다.
+3. useModal() 훅을 사용한다.
+4. useModal()을 destructing해서 {isOpen, open, close}를 가져온다. {isOpen, open, close} = useModal();
+5. 모달 컴포넌트에 isOpen, close를 Props로 내려준다. isOpen={isOpen}, onClose(close)
+6. open은 모달을 열고 싶은 버튼에 onClick 이벤트로 넣어준다.
+7. 추가로 props로 title, button, extra를 보내줄 수 있다.
+*/
+
+const Modal = ({ title, button, children, extra, isOpen, onClose }) => {
   if (!isOpen) return;
 
   return createPortal(
     <div className='modal-overlay fixed top-0 right-0 bottom-0 left-0 flex h-screen w-screen items-center justify-center bg-black/40'>
-      <div className='modal-content flex aspect-square w-9/10 max-w-1/5 flex-col bg-sky-950 p-10 text-white'>
-        <div className='flex justify-between'>
-          {title && <p>{title}</p>}
-          <button onClick={onClose}>&times;</button>
+      <div className='modal-content flex w-9/10 max-w-md min-w-64 flex-col bg-black p-10 text-white'>
+        <div className='relative flex justify-between'>
+          {title && <p className='content-text'>{title}</p>}
+          <button
+            className='content-text absolute -top-6 right-0 text-white'
+            onClick={onClose}
+          >
+            &times;
+          </button>
         </div>
-        <div className='flex h-8/10 items-center justify-center'>
-          {children}
-        </div>
+        <div className='my-4 flex items-center justify-center'>{children}</div>
         <button>버튼 컴포넌트 가져오기</button>
-        {extra && <span className='text-center'>{extra}</span>}
+        {extra && (
+          <span className='caption-text mt-2 text-center'>{extra}</span>
+        )}
       </div>
     </div>,
     document.body,
--- src/pages/landing-page/index.jsx
+++ src/pages/landing-page/index.jsx
@@ -1,12 +1,16 @@
 import Modal from '@components/common/Modal';
+import useModal from '@hooks/useModal';
 
 export default function LandingPage() {
   const { isOpen, open, close } = useModal();
   return (
     <div>
       Landing
-      <button>모달 오픈</button>
-      <Modal isOpen={true}>모달</Modal>
+      <button onClick={open}>모달 오픈</button>
+      <Modal isOpen={isOpen} onClose={close}>
+        모달은 그러하다. 재미있고 재미있지 않고 아무렇지도 않기도 하고 크기에
+        따라 달라진다면 과연 크기는 얼마나가 되어야할까
+      </Modal>
     </div>
   );
 }
