d8ef7b15206b338be20e645c015e766ebd3ea78f
Yongmin0423 <yym21345@gmail.com>
2025-04-18 17:55:24 +0900

    fix(tab): 테스트를 위해 주석처리 했던 부분 초기화

--- src/pages/main-page/components/Idol-list-tab-content.jsx
+++ src/pages/main-page/components/Idol-list-tab-content.jsx
@@ -14,9 +14,9 @@ const mockData = [
 
 const IdolListTabContent = ({ idolData, IdolList }) => {
   //idolData가 제대로 불러오지 않았을 경우 처리
-  // if (!idolData || idolData.length === 0) {
-  //   return <div>아이돌 정보가 없습니다.</div>;
-  // }
+  if (!idolData || idolData.length === 0) {
+    return <div>아이돌 정보가 없습니다.</div>;
+  }
   return (
     <>
       <div className='grid w-full grid-cols-1 gap-5 border-2 md:grid-cols-2'>
