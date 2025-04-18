d323dd462c288718f5074e75cef6fdabbe0a1151
Yongmin0423 <yym21345@gmail.com>
2025-04-18 19:35:50 +0900

    feat(header): 헤더 기초 레이아웃 구현

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
 
 export default function Header() {
-  return <header>헤더</header>;
+  return (
+    <header className='grid h-[5rem] w-full grid-cols-3 bg-black sm:h-[8rem]'>
+      <div></div>
+      <div className='flex items-center justify-center'>
+        <img
+          src='src\assets\images\fandom-k-logo.webp'
+          alt='Fandom-K logo'
+          className='h-[2rem]'
+        />
+      </div>
+      <div className='flex items-center justify-center'>
+        <img
+          src='src\assets\images\empty-profile.webp'
+          alt='empty profile Img'
+          className='h-[2rem] w-[2rem] rounded-full'
+        />
+      </div>
+    </header>
+  );
 }
