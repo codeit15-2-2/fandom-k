2fbe23ddf49828a079b38a0048883791cb18bc12
Yongmin0423 <yym21345@gmail.com>
2025-04-18 18:06:15 +0900

    feat(tab): tailwind 조건부 클래스에 cn 함수 사용

--- src/pages/main-page/components/Idol-list-tab.jsx
+++ src/pages/main-page/components/Idol-list-tab.jsx
@@ -1,3 +1,4 @@
+import { cn } from '@utils/cn';
 import { Link, Navigate, Outlet, useMatch } from 'react-router-dom';
 
 const IdolListTab = () => {
@@ -11,15 +12,21 @@ const IdolListTab = () => {
   }
 
   return (
-    <div class='flex h-full max-w-5/8 flex-col bg-black'>
+    <div className='flex h-full max-w-5/8 flex-col bg-black'>
       <div className='sub-content-text flex h-[4.2rem] w-full items-center justify-between text-white'>
         <div
-          class={`flex h-full w-1/2 items-center justify-center ${femaleMatch ? 'border-b border-white bg-white/10' : ''}`}
+          className={cn(
+            'flex h-full w-1/2 items-center justify-center',
+            femaleMatch ? 'border-b border-white bg-white/10' : '',
+          )}
         >
           <Link to='female'>이달의 여자 아이돌</Link>
         </div>
         <div
-          class={`flex h-full w-1/2 items-center justify-center ${maleMatch ? 'border-b border-white bg-white/10' : ''}`}
+          className={cn(
+            'flex h-full w-1/2 items-center justify-center',
+            femaleMatch ? 'border-b border-white bg-white/10' : '',
+          )}
         >
           <Link to='male'>이달의 남자 아이돌</Link>
         </div>
