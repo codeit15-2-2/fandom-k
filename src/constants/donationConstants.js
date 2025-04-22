0f6dcf03c812e5cb207447087725cd324e46f4a1
MyungJiwoo <1206jiwoo@gmail.com>
2025-04-23 00:49:19 +0900

    #42 refactor(donationInfo): 스타일 상수와 유틸 함수 분리

---
+++ src/constants/donationConstants.js
@@ -0,0 +1,17 @@
+export const DONATION_INFO_CONTENT_SIZE_STYLES = {
+  s: 'text-5xl',
+  m: 'text-6xl',
+  l: 'text-8xl',
+};
+
+export const DONATION_INFO_SUB_CONTENT_SIZE_STYLES = {
+  s: 'text-3xl',
+  m: 'text-4xl',
+  l: 'text-6xl',
+};
+
+export const DONATION_INFO_WIDTH_STYLES = {
+  s: 'w-[25rem]',
+  m: 'w-[30rem]',
+  l: 'w-[50rem]',
+};
