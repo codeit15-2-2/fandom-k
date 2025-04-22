0a4c2e58d80d455a28dbb6e840e66f0f8d65659f
popo <hirouth@naver.com>
2025-04-22 23:29:12 +0900

    styles: 전체 섹션 1차 구현 완료

--- src/pages/landing-page/sections/CreditSection.jsx
+++ src/pages/landing-page/sections/CreditSection.jsx
@@ -8,7 +8,7 @@ const CreditSection = () => {
     <section className={cn(SNAP_ITEM, 'relative overflow-hidden bg-black')}>
       {/* Background Video */}
       <video
-        className='absolute inset-0 z-0 h-full w-full object-cover'
+        className='absolute inset-0 z-0 h-[100%] object-contain'
         loop
         autoPlay
         muted
@@ -17,12 +17,12 @@ const CreditSection = () => {
       </video>
 
       {/* Title */}
-      <div className='relative z-10 mb-10 flex h-[30vh] items-end justify-end px-16 text-[15rem] font-semibold text-white'>
+      <div className='z-10 flex h-[30vh] items-center justify-center text-[15rem] font-semibold text-white'>
         <h1>FANDOM-K</h1>
       </div>
 
       {/* CTA Content */}
-      <div className='relative z-10 mb-40 flex flex-col items-center justify-center gap-12 py-32 text-6xl text-white'>
+      <div className='z-10 flex flex-col items-center justify-center gap-12 text-6xl text-white'>
         <p>당신의 사랑을 FANDOM-K가 함께합니다</p>
         <Button>크레딧 받고 시작하기</Button>
       </div>
--- src/pages/landing-page/sections/DonationSection.jsx
+++ src/pages/landing-page/sections/DonationSection.jsx
@@ -1,7 +1,7 @@
 import { cn } from '@libs/cn';
 import { SNAP_ITEM } from '@pages/landing-page/constants/layouts';
 import handphone from '@assets/elements/elem_handphone.png';
-
+import heart from '@assets/icons/icon_heart.svg';
 const DonationSection = () => {
   return (
     <section
@@ -12,12 +12,13 @@ const DonationSection = () => {
     >
       <div className='relative mx-auto flex h-full w-[1400px] flex-col'>
         {/* 제목 컨테이너 */}
-        <header className='flex flex-1 items-center justify-center'>
+        <header className='relative flex flex-1 items-center justify-center'>
           <h1 className='text-center text-[15rem]/50 font-extrabold tracking-tight text-white'>
             EASY
             <br />
             DONATION
           </h1>
+          <img src={heart} className='absolute bottom-1/4 w-[10%]' />
         </header>
 
         {/* 콘텐츠 컨테이너 */}
@@ -32,10 +33,10 @@ const DonationSection = () => {
             최애와
             <br />나
           </p>
-          <img
-            src={handphone}
-            className='absolute bottom-0 left-1/3 -translate-x-1/3'
-          />
+
+          <div className='absolute bottom-0 left-1/3 flex -translate-x-1/3 flex-col items-center gap-4'>
+            <img src={handphone} />
+          </div>
         </div>
       </div>
     </section>
