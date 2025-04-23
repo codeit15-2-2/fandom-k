c7b5f1614f208bd56904b1241b608d0947f89fbe
evaain706 <evaain706@gmail.com>
2025-04-24 00:39:45 +0900

    #33 style(MyPage):App.js 원상복구

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Footer from '@components/layouts/Footer';
 import Header from '@components/layouts/Header';
-import MyPage from '@pages/my-page';
 import { Outlet } from 'react-router-dom';
 
 export default function App() {
   return (
-    <>
-      <MyPage />
-    </>
+    <div>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   );
 }
