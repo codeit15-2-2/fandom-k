312f430d57c91d5949d32acd86991db514183f03
evaain706 <evaain706@gmail.com>
2025-04-23 17:13:22 +0900

    #36 chore(DonateModal): 테스트용으로 수정했던 App.js 원상복구

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import Footer from '@components/layouts/Footer';
 import Header from '@components/layouts/Header';
-import DetailPage from '@pages/detail-page';
 import { Outlet } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
       <Header />
-
-      <DetailPage />
       <main>
         <Outlet />
       </main>
       <Footer />
-
-      <DetailPage />
     </div>
   );
 }
