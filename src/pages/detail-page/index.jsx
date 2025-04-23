1c565c900c6571d45d1653b96ea41aacd208224e
evaain706 <evaain706@gmail.com>
2025-04-23 17:07:52 +0900

    #36 chore(DonateModal):테스트용 index.js 초기화

--- src/pages/detail-page/index.jsx
+++ src/pages/detail-page/index.jsx
@@ -1,33 +1,3 @@
-import { useState } from 'react';
-import DonateModal from './DonateModal';
-
 export default function DetailPage() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const openModal = () => setIsOpen(true);
-  const closeModal = () => setIsOpen(false);
-
-  return (
-    <div className='p-8'>
-      <button
-        className='rounded bg-blue-500 px-4 py-2 text-white'
-        onClick={openModal}
-      >
-        후원 모달 열기
-      </button>
-
-      <DonateModal
-        isOpen={isOpen}
-        close={closeModal}
-        donateId={123}
-        cardItem={{
-          id: 3,
-          title: '르세라핌 하이하이',
-          location: '강남역 광고',
-        }}
-        credit={10000}
-        isDonate
-      />
-    </div>
-  );
+  return <div>Detail</div>;
 }
