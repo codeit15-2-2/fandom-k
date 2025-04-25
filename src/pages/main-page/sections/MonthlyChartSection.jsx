a328a6a97ad7596ed428476b6302f41432ec6887
hizdev <gpfksdlrn@naver.com>
2025-04-26 02:45:50 +0900

    #79 refactor: 컴포넌트명과 파일명 통일

--- src/pages/main-page/sections/MonthlyChartSection.jsx
+++ src/pages/main-page/sections/MonthlyChartSection.jsx
@@ -1,4 +1,4 @@
-import MonthlyChartHeader from '../components/ChartHeader';
+import MonthlyChartHeader from '../components/MonthlyChartHeader';
 import MonthlyChartTabs from '../components/MonthlyChartTabs';
 
 const MonthlyChartSection = () => {
