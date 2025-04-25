13d59912d559abe12b30a0bb05f1cfea2add15c8
popo <hirouth@naver.com>
2025-04-25 15:47:58 +0900

    design: credit 섹션 반응형 구현

--- src/pages/landing-page/components/common/Button.jsx
+++ src/pages/landing-page/components/common/Button.jsx
@@ -4,7 +4,12 @@ const Button = ({ children }) => {
   return (
     <button
       className={cn(
-        `px:18 w-80 cursor-pointer rounded-full bg-gradient-to-r from-pink-500 to-rose-400 py-2 text-3xl font-semibold text-white shadow-lg/40 transition-colors duration-300 hover:from-pink-300 hover:to-rose-200 md:w-130 md:px-24 md:py-4`,
+        // Base styles
+        'w-90 cursor-pointer rounded-full bg-gradient-to-r from-pink-500 to-rose-400 px-12 py-4 text-2xl font-semibold text-white shadow-lg/40 transition-colors duration-300',
+        // Hover styles
+        'hover:from-pink-300 hover:to-rose-200',
+        // Responsive overrides
+        'md:w-130 md:px-24 md:py-6 md:text-3xl',
       )}
     >
       {children}
