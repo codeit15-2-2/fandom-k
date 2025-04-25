fd109d5d9021ad157fb2e600296915477d744034
MyungJiwoo <1206jiwoo@gmail.com>
2025-04-25 17:40:57 +0900

    #56 refactor(MainTitle): 후원 상세페이지의 Main Title의 반응형 폰트 크기 삭제

--- src/pages/detail-page/components/MainTitle.jsx
+++ src/pages/detail-page/components/MainTitle.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, memo } from 'react';
 import { cn } from '@/utils/cn';
 
 const MAIN_TITLE_SIZE_STYLES = {
@@ -7,11 +7,17 @@ const MAIN_TITLE_SIZE_STYLES = {
   l: 'text-5xl',
 };
 
+const MAIN_NAME_SIZE_STYLES = {
+  s: 'text-[10rem]',
+  m: 'text-[10rem]',
+  l: 'text-[15rem]',
+};
+
 /**
  * 후원 제목 컴포넌트 (메인)
  * @component
  * @param {string} props.title - 후원 제목
- * @param {string} props.name - 아이돌 그룹명 + 이름
+ * @param {string} props.name - 아이돌 영문 이름
  * @param {'s' | 'm' | 'l'} [props.size='s'] - 후원 제목 사이즈 (default size: 's')
  *
  * @example
@@ -19,44 +25,25 @@ const MAIN_TITLE_SIZE_STYLES = {
  */
 
 const MainTitle = ({ title, name, size = 's' }) => {
-  const containerRef = useRef(null);
-  const textRef = useRef(null);
-  const [fontSize, setFontSize] = useState(100);
-
-  // 이름이 부모 컨테이너의 width에 꽉 차도록 폰트 사이즈 수정하기 (반응형 단위를 사용할 경우 텍스트가 넘칠 수 있다.)
-  useEffect(() => {
-    const containerWidth = containerRef.current.offsetWidth;
-    const textWidth = textRef.current.scrollWidth;
-
-    const ratio = containerWidth / textWidth;
-    const newFontSize = Math.floor(100 * Math.min(ratio, 1.7)); // 최대 크기 지정
-
-    setFontSize(newFontSize);
-  }, [name]);
-
-  const titleClassNames = cn('mb-2', MAIN_TITLE_SIZE_STYLES[size]);
+  const nameClassNames = cn(
+    'font-extralight break-words',
+    MAIN_NAME_SIZE_STYLES[size],
+  );
 
   return (
-    <div className='w-[60vw] text-white'>
-      <p className={titleClassNames}>{title}</p>
-      <div
-        ref={containerRef}
-        className='h-fit w-full font-thin tracking-tighter'
-      >
-        <p
-          ref={textRef}
-          style={{
-            fontSize: `${fontSize}px`,
-            whiteSpace: 'nowrap',
-            display: 'inline-block',
-            lineHeight: '0.75',
-          }}
-        >
-          {name}
-        </p>
+    <div className='w-full text-white'>
+      <p className={MAIN_TITLE_SIZE_STYLES[size]}>{title}</p>
+      <div className='w-full leading-none'>
+        <p className={nameClassNames}>{name}</p>
       </div>
     </div>
   );
 };
 
 export default MainTitle;
+// export default memo(MainTitle, (prev, next) => {
+//   if (prev.title !== next.title) return false;
+//   if (prev.name !== next.name) return false;
+//   if (prev.size !== next.size) return false;
+//   return true;
+// });
