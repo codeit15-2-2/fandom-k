13ba79aaa3b5c3a811fca592c57185248c724e0f
MyungJiwoo <1206jiwoo@gmail.com>
2025-04-26 00:38:55 +0900

    #56 refactor: alt 설명 구체화

--- src/pages/detail-page/components/BackgroundIdolImage.jsx
+++ src/pages/detail-page/components/BackgroundIdolImage.jsx
@@ -1,7 +1,11 @@
 const BackgroundIdolImage = ({ imgSrc }) => {
   return (
     <div className='absolute inset-0 hidden md:block'>
-      <img src={imgSrc} alt='idol' className='h-full w-full object-cover' />
+      <img
+        src={imgSrc}
+        alt='아이돌 이미지'
+        className='h-full w-full object-cover'
+      />
 
       <div className='absolute inset-0 bg-gradient-to-r from-transparent to-black/80'></div>
       <div className='absolute inset-0 bg-gradient-to-b from-transparent via-black/30 to-black'></div>
