d6da37cb688bded7cd28d5c56fab4db6fbc14240
Yongmin0423 <yym21345@gmail.com>
2025-04-25 12:44:42 +0900

    style:마이 크레딧 위 아래 마진 추가

--- src/pages/main-page/components/MyCredit.jsx
+++ src/pages/main-page/components/MyCredit.jsx
@@ -3,7 +3,7 @@ import Jewel from '@assets/icons/icon_jewel';
 const MyCredit = ({ credit, open }) => {
   return (
     <>
-      <div className='flex h-[8.5rem] w-full max-w-5/8 items-center justify-between rounded-xl border border-white bg-black px-10 sm:h-[13rem]'>
+      <div className='my-10 flex h-[8.5rem] w-full max-w-5/8 items-center justify-between rounded-xl border border-white bg-black px-10 sm:h-[13rem]'>
         <div className='flex flex-col justify-center'>
           <p className='caption-text text-white/60 sm:text-[1.6rem]'>
             내 크레딧
