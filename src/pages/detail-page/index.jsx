dd4d6b09fa1dc5d60863a61f2cae2865f3cdaebf
MyungJiwoo <1206jiwoo@gmail.com>
2025-04-28 02:26:24 +0900

    #104 bug: 상세 페이지에서 유효한 url인지 검사 후 404 페이지로 이동

--- src/pages/detail-page/index.jsx
+++ src/pages/detail-page/index.jsx
@@ -1,4 +1,4 @@
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import useDeviceSize from '@hooks/useDeviceSize';
 import BackgroundIdolImage from '@pages/detail-page/components/BackgroundIdolImage';
 import PCMainSection from '@pages/detail-page/sections/pc/MainSection';
@@ -8,15 +8,23 @@ import MobileDetailSection from '@pages/detail-page/sections/mobile/DetailSectio
 import donationDetailData from '@/mocks/donationDetailData.json';
 import { useDonation } from '@contexts/DonationContext';
 
+const validatePath = (donationData) => {
+  const lastSegment = location.pathname.split('/').filter(Boolean).at(-1);
+  return donationData.id === Number(lastSegment);
+};
+
 export default function DetailPage() {
   const { isDesktop, isTablet, isMobile } = useDeviceSize();
   const { donationData, isLoading } = useDonation();
 
+  // context data는 원래 복원 전과 복원 후로 무조건 null이 한 번 출력되고, 그 다음에 데이터가 채워져서 정상 작동한다. => 데이터 null 방지용으로 isLoading 추가
   if (isLoading) {
     return <div className='text-white'>Loading...</div>;
   }
 
-  if (!donationData) {
+  // 유효한 url인지 검증
+  const isValidPath = validatePath(donationData);
+  if (!isValidPath) {
     return <Navigate to='/error/404' />;
   }
 
