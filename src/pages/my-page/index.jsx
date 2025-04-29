181e812ee0a2b5d369bbf8af809c44d5112c86f0
popo <hirouth@naver.com>
2025-04-29 17:33:24 +0900

    #125 fix: Header 및 Footer 수정, 관련 파일 수정

--- src/pages/my-page/index.jsx
+++ src/pages/my-page/index.jsx
@@ -31,7 +31,7 @@ export default function MyPage() {
     });
 
   return (
-    <div className='min-h-screen w-full bg-black text-white'>
+    <div className='relative min-h-screen w-full bg-black text-white'>
       <div className='mx-auto max-w-[140rem] rounded-2xl px-6 md:px-6 lg:px-6 xl:px-48'>
         <FavoriteListSection
           favorites={favorites}
